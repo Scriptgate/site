9643bde848d3db063a3d6d29f6276e3afb2b4aaf
ventouristeam <ventouristeam@gmail.com>
2018-11-12 17:24:11 +0100

    hexagonal index page

--- src/website/js/script.js
+++ src/website/js/script.js
@@ -1,23 +1,21 @@
-window.addEventListener('load', onLoad)
-
+window.addEventListener('load', onLoad);
+window.addEventListener('scroll', onScroll);
 
 function onLoad() {
 }
 
 var opening = 0;
+var logoWidth = 446;
 
 function openTheGates() {
-
-
-
     opening+=5;
 
-	var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
-	var newWidth = 446 + opening;
+	var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+	var newWidth = logoWidth + opening;
 	var marginLeft = Math.floor((newWidth - gid('container').clientWidth - scrollbarWidth)/2.0);
 
-	if(newWidth < 446)  {
-		newWidth = 446;
+	if(newWidth < logoWidth)  {
+		newWidth = logoWidth;
 		marginLeft = 0;
 	}
 
@@ -26,13 +24,26 @@ function openTheGates() {
     logo.style.marginLeft = '-'+marginLeft+'px';
     logo.style.width = newWidth+'px';
 
-    console.log("window:"+window.innerWidth);
-    console.log("container:"+logo.clientWidth);
     if(logo.clientWidth < window.innerWidth ) {
         setTimeout(function(){ openTheGates(); }, 10);
     } else {
         window.location.href = "http://scriptgate.net/html/site.html";
     }
+}
+
+function onScroll() {
+    var scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    var newWidth = logoWidth + pageYOffset;
+    var marginLeft = Math.floor((newWidth - gid('container').clientWidth - scrollbarWidth)/2.0);
+
+    if(newWidth < logoWidth)  {
+        newWidth = logoWidth;
+        marginLeft = 0;
+    }
+    var logo = gid('logo_container');
+
+    logo.style.marginLeft = '-'+marginLeft+'px';
+    logo.style.width = newWidth+'px';
 
 }
 
